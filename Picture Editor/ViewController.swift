65028e2b98bbb5aa4a3cc6e38a807412f31aba9c
Fabian Hernandez Aguilar <fhaguilar2019@208-Engineering-17.local>
2019-02-06 13:20:19 -0800

    Initial Commit

---
+++ Picture Editor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Picture Editor
+//
+//  Created by Fabian Hernandez Aguilar on 2/6/19.
+//  Copyright © 2019 Fabian Hernandez Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Picture EditorTests/Picture_EditorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Picture_EditorTests.swift
+//  Picture EditorTests
+//
+//  Created by Fabian Hernandez Aguilar on 2/6/19.
+//  Copyright © 2019 Fabian Hernandez Aguilar. All rights reserved.
+//
+
+import XCTest
+@testable import Picture_Editor
+
+class Picture_EditorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
